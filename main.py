c9c41e72661f1db675cb1e863fbcfb10fec2a9d7
Иванов Никита <pika9969065@gmail.com>
2024-02-10 12:03:19 +0300

    Изображение Марса

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -14,6 +14,28 @@ def promotion():
     litss = ['Человечество вырастает из детства.', 'Человечеству мала одна планета.',
              'Мы сделаем обитаемыми безжизненные пока планеты.', 'И начнем с Марса!', 'Присоединяйся!']
     return '</br>'.join(litss)
+
+
+@app.route('/image_mars')
+def image_mars():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                        crossorigin="anonymous">
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/MARS.png')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-primary" role="alert">
+                          Вот она какая, красная планета.
+                        </div>
+                      </body>
+                    </html>'''
 
 
 if __name__ == '__main__':
